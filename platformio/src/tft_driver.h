bfb7c14e6a012cb14950ec3d2c084b0786273b74
Zapta <zapta@users.noreply.github.com>
2021-06-05 12:02:20 -0700

    Code clearnup. Refactored as a namespace with an implicit singleton.

--- platformio/src/tft_driver.h
+++ platformio/src/tft_driver.h
@@ -10,4 +10,7 @@ void begin();
 void fill_rect(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2,
                uint16_t color);
 
+// For testing.
+bool is_overrun();
+
 }  // namespace tft_driver
